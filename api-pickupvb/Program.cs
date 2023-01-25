7f285a773efe1af0f50c47acede1abc7e29c0f8a
Zachary Lockhart <zacharyjordan82@gmail.com>
2023-01-25 14:50:17 -0500

    adding cors with allowed origins

--- api-pickupvb/Program.cs
+++ api-pickupvb/Program.cs
@@ -13,7 +13,22 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+var  allowedOrigins = "_allowOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: allowedOrigins, policy  =>
+    {
+        policy.WithOrigins(
+            "https://pickupvb.com",
+            "http://pickupvb.com",
+            "https://www.pickupvb.com",
+            "http://www.pickupvb.com",
+            "http://localhost:4200",
+            "https://localhost:4200"
+        ).AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,6 +38,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(allowedOrigins);
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
